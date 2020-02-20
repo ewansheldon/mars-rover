f5913f4f24eaa5cff9e97b87ad09a3318e768817
Ewan <ewan.s@cucumberwifi.io>
2020-02-20 15:54:00 +0000

    command character constants

--- src/main/java/ewansheldon/kata/mars_rover/CommandControl.java
+++ src/main/java/ewansheldon/kata/mars_rover/CommandControl.java
@@ -3,6 +3,9 @@ package ewansheldon.kata.mars_rover;
 import java.util.Map;
 
 public class CommandControl {
+    public static final char MOVE = 'M';
+    public static final char LEFT = 'L';
+    public static final char RIGHT = 'R';
     private Map<Character, Command> commands;
     private Orientation orientation;
     private Thrust thrust;
@@ -15,9 +18,9 @@ public class CommandControl {
 
     private void createCommands() {
         this.commands = Map.of(
-                'M', new MoveForward(thrust),
-                'L', new TurnLeft(orientation),
-                'R', new TurnRight(orientation)
+                MOVE, new MoveForward(thrust),
+                LEFT, new TurnLeft(orientation),
+                RIGHT, new TurnRight(orientation)
         );
     }
 
